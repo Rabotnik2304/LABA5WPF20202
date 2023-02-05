f403ed3bcc4b60a839c206321e3ef95a708bc8a3
informant <artemiy.solodkin@mail.ru>
2023-02-05 20:06:40 +0500

    Final

--- LABA5WPF20202/MainWindow.xaml.cs
+++ LABA5WPF20202/MainWindow.xaml.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Forms;
-using Newtonsoft.Json;
 using System;
-using System.Windows.Input;
-using System.Windows.Documents;
 using System.Windows.Controls;
 using Newtonsoft.Json.Schema;
 using static LABA5WPF20202.MainWindow;
@@ -25,7 +22,7 @@ namespace LABA5WPF20202
         {
             InitializeComponent();
         }
-        
+
         private void MenuOpenFileClick(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog openFolderDialog = new FolderBrowserDialog();
@@ -44,57 +41,72 @@ namespace LABA5WPF20202
                 if (filePath.Substring(filePath.Length - 4, 4) == "json")
                 {
                     Scheme schemeOfTable = Scheme.ReadJson(filePath);
-                    
-                    string tableName = schemeOfTable.Name;                    
-                    string pathTable = filePath.Substring(0, filePath.Length - 12)+".csv";
-                    
-                    Table table = TableReader.TableRead(schemeOfTable, pathTable);
-                    
 
-                    schemesAndTablesDict.Add(schemeOfTable,table);
-                    
+                    string tableName = schemeOfTable.Name;
+                    string pathTable = filePath.Substring(0, filePath.Length - 12) + ".csv";
+
+                    Table table = TableReader.TableRead(schemeOfTable, pathTable);
+
+                    schemesAndTablesDict.Add(schemeOfTable, table);
+
                     TreeViewItem tableTree = new TreeViewItem();
 
                     tableTree.Header = tableName;
-                    
+
                     tableTree.Selected += TableTreeSelected;
-                    
+
                     tableTree.Unselected += TableTreeUnselected;
-                    
+
                     foreach (SchemeColumn key in schemeOfTable.Columns)
                     {
-                        tableTree.Items.Add(key.Name +"---"+ key.Type);
+                        tableTree.Items.Add(key.Name + "---" + key.Type);
                     }
 
                     dataTree.Items.Add(tableTree);
                 }
-            }    
+            }
         }
-
+        private class RowAdapter
+        {
+            public List<Object> Data { get; set; }
+        }
         private void TableTreeSelected(object sender, RoutedEventArgs e)
         {
             DataTable.Columns.Clear();
             string tableName = ((TreeViewItem)sender).Header.ToString();
 
-            foreach(var schemeAndTable in schemesAndTablesDict)
+            foreach (var schemeAndTable in schemesAndTablesDict)
             {
-                if (schemeAndTable.Key.Name==tableName)
+                if (schemeAndTable.Key.Name == tableName)
                 {
-                    
-                    foreach (SchemeColumn column in schemeAndTable.Key.Columns)
+
+                    List<RowAdapter> rowsData = new List<RowAdapter>();
+
+                    foreach (Row row in schemeAndTable.Value.Rows)
                     {
-                        DataGridTextColumn tableTextColumn = new DataGridTextColumn();
-                        tableTextColumn.Header = column.Name;
-                        DataTable.Columns.Add(tableTextColumn);
-                        
-                        foreach (Row row in schemeAndTable.Value.Rows)
+                        List<object> rowData = new List<object>();
+                        foreach (object cell in row.Data.Values)
                         {
-                             DataTable.Items.Add("12");
-                        }    
+                            rowData.Add(cell);
+                        }
+                        rowsData.Add(new RowAdapter() { Data = rowData });
+                    }
+
+                    DataTable.ItemsSource = rowsData;
+
+                    for (int i = 0; i < schemeAndTable.Key.Columns.Count; i++)
+                    {
+                        DataGridTextColumn tableTextColumn = new DataGridTextColumn()
+                        {
+                            Header = schemeAndTable.Key.Columns[i].Name,
+                            Binding = new System.Windows.Data.Binding($"Data[{i}]")
+                        };
+
+                        DataTable.Columns.Add(tableTextColumn);
                     }
                     break;
                 }
-            }   
+            }
         }
         private void TableTreeUnselected(object sender, RoutedEventArgs e)
         {
